dbf512841b9cf08bce7de10324bda7f95cc694ed
maodouchat <148543932+maodouchat@users.noreply.github.com>
2023-11-13 10:24:55 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,13 @@
-const boardElement = document.getElementById('board');
+document.addEventListener('DOMContentLoaded', () => {
+    const boardElement = document.getElementById('board');
 
-// 初始化棋盘
-for (let i = 0; i < 225; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    boardElement.appendChild(cell);
-}
-
-// 添加点击事件监听器
-boardElement.addEventListener('click', (event) => {
-    // 在这里添加放置棋子的逻辑
+    // 初始化棋盘
+    for (let i = 0; i < 225; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.addEventListener('click', () => {
+            cell.classList.add('occupied');
+        });
+        boardElement.appendChild(cell);
+    }
 });
